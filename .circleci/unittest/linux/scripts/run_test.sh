4077377b5916a6e4614376ddb5268dc1e3faef24
Sam Andow <samdow@fb.com>
2021-09-30 16:33:17 +0000

    add better test results

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+. venv/bin/activate
+mkdir test-results
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
@@ -9,4 +11,4 @@ python -m torch.utils.collect_env
 
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
-pytest -v --ignore=test/test_functorch_lagging_op_db.py test/ || exit 255
+pytest -v --ignore=test/test_functorch_lagging_op_db.py --junitxml=test-results/junit.xml test/ || exit 255
